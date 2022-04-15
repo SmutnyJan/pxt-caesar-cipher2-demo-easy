ae6258ec8ef51b8811ef0ffc0a76d3ca5d0b3982
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-04-15 17:21:37 +0200

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-
+input.onButtonPressed(Button.A, function () {
+    radio.sendString(cipher.decryptText("microbit", 5))
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(cipher.encryptText(receivedString, 5))
+})
